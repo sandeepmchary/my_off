2cb7d3613d6623a6f31fdd6607f8f3ccd92567de
sandeepmchary <awssandeepchary@gmail.com>
2021-10-31 12:15:48 +0530

    added hari kamma terraform

--- Notes/Hari_kammana_terraform/Code/main.tf
+++ Notes/Hari_kammana_terraform/Code/main.tf
@@ -2,16 +2,35 @@ provider "aws"{
     profile = "terra"
     region = "us-east-2"
 }
+terraform{
+    backend "s3" {
+        bucket = "javahomesam"
+        key = "terraform.tfstate"
+        region = "us-east-2"
+        dynamodb_table = "javahomesam" 
+    }
+}
 resource "aws_vpc" "javahome" {
   cidr_block = "10.0.0.0/16"
   instance_tenancy = "default"
   tags = {
     "Name" = "JavaHOme"
+    "Env" = "Dev"
   }
+}
+resource "aws_subnet" "javahomesubnet" {
+  vpc_id = aws_vpc.javahome.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "us-east-2b"
+}
+resource "aws_subnet" "javahomesubnet1" {
+  vpc_id = aws_vpc.javahome.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-2c"
 }
 output "javahomecidr" {
   value = "${aws_vpc.javahome.cidr_block}"
-}
-output "vpc_id_value" {
+  }
+output "Vpc_id_value" {
   value = "${aws_vpc.javahome.id}"
 }
